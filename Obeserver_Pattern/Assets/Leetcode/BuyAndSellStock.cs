7d5b9b5a8d3d1626e8e312792813a5768f4af55b
aashu1208 <86849270+aashu1208@users.noreply.github.com>
2025-05-05 16:06:19 +0530

    Buy and sell stock programme

--- Obeserver_Pattern/Assets/Leetcode/BuyAndSellStock.cs
+++ Obeserver_Pattern/Assets/Leetcode/BuyAndSellStock.cs
@@ -7,7 +7,7 @@ public class BuyAndSellStock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Debug.Log(MaxProfit(new int[]{7,1,5,3,6,4}));
     }
 
     // Update is called once per frame
@@ -18,9 +18,19 @@ public class BuyAndSellStock : MonoBehaviour
 
     public int MaxProfit(int[] prices){
 
+        int minPrice = int.MaxValue;
+        int maxProfit = 0;
 
-
-
-        return 0;
+        for(int i = 0; i<prices.Length; i++){
+            if(prices[i]<minPrice){
+                minPrice = prices[i];
+            }
+            else
+            {
+                int profit = prices[i] - minPrice;
+                maxProfit = profit;
+            }
+        }
+        return maxProfit;
     }
 }
