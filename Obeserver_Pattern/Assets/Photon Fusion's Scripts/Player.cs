dce4cefd25f5f2f3e5b913ba94a2b3801e8af6fa
aashu1208 <86849270+aashu1208@users.noreply.github.com>
2025-05-09 19:00:23 +0530

    shared mode implemented and tested

--- Obeserver_Pattern/Assets/Photon Fusion's Scripts/Player.cs
+++ Obeserver_Pattern/Assets/Photon Fusion's Scripts/Player.cs
@@ -1,13 +1,15 @@
 using Fusion;
+using TMPro;
 using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
     private NetworkCharacterController _cc;
-    public Transform cube;
+    
+    private TMP_Text _messages;
+    public bool isBigLocal = false;
 
-    [Networked] private bool isBig { get; set; } = false;
-
+    public GameObject cube;
     private void Awake()
     {
         _cc = GetComponent<NetworkCharacterController>();
@@ -21,11 +23,79 @@ public class Player : NetworkBehaviour
             _cc.Move(5 * data.direction * Runner.DeltaTime);
         }
 
-        if (HasInputAuthority && Input.GetKeyDown(KeyCode.G))
+        
+    }
+
+    public void Update()
+    {
+
+        if(Object.HasInputAuthority)
         {
-            isBig = !isBig;  // ✅ Only this player's cube will toggle size
+
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+
+                RPC_SendMessage("Hey mate!");
+            }
+
+            // Scale-Descale Cube
+            if(Input.GetKeyDown(KeyCode.UpArrow))
+            {
+
+                RPC_UpdateScale(1.5f);
+            }
+
+            else if(Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                RPC_UpdateScale(0.5f);
+            }
+            
+            
+        }
+    }
+    
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    public void RPC_UpdateScale(float scaleMultiplier)
+    {
+        RPC_ScaleCube(scaleMultiplier);
+    }
+
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    public void RPC_ScaleCube(float scaleMultiplier)
+    {
+
+        if(cube != null)
+        {
+            // Scale the cube
+            cube.transform.localScale *= scaleMultiplier;
+        }
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority, HostMode = RpcHostMode.SourceIsHostPlayer)]
+    public void RPC_SendMessage(string message, RpcInfo info = default)
+    {
+        RPC_RelayMessage(message, info.Source);
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All, HostMode = RpcHostMode.SourceIsServer)]
+    public void RPC_RelayMessage(string message, PlayerRef messageSource)
+    {
+        if (_messages == null)
+            _messages = FindObjectOfType<TMP_Text>();
+
+        if (messageSource == Runner.LocalPlayer)
+        {
+            message = $"You said: {message}\n";
+        }
+        else
+        {
+            message = $"Some other player said: {message}\n";
         }
 
-        cube.localScale = isBig ? Vector3.one * 2f : Vector3.one;
+        _messages.text += message;
+
+        
     }
 }
